d5ff46f904330f2a5963310e9d8c882878b968f0
lm <1790008590@qq.com>
2021-09-15 14:21:10 +0800

    2

--- caculate.cs
+++ caculate.cs
@@ -1,1 +1,18 @@
-public
+public static class caculate
+{
+	public int add(int a,int b)
+	{
+		return a+b;
+	}
+	public int max(int a,int b)
+	{
+		if(a>b)
+			return a;
+		else 
+			return b;
+	}
+	public int jian(int a,int b)
+	{
+		return a-b;
+	}
+}
